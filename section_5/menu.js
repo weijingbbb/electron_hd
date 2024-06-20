79b124183a1cd36a631a80f4fd5ccc6647f63b6d
weijingbbb <1047129443@qq.com>
2024-06-21 03:00:53 +0800

    基础：第六章，弹窗的基本使用。

--- section_5/menu.js
+++ section_5/menu.js
@@ -1,4 +1,4 @@
-const { app, Menu, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow, dialog } = require('electron');
 
 const createMenu = (win) => {
 
@@ -43,7 +43,20 @@ const createMenu = (win) => {
                 },
                 {
                     label: isMac ? '关闭' : '退出',
-                    role: isMac ? 'close' : 'quit'
+                    // role: isMac ? 'close' : 'quit',
+                    click: async () => {
+                        const { response, checkboxChecked } = await dialog.showMessageBox({
+                            title: app.name,
+                            detail: '晚上去唱k吗？',
+                            // 尽量固定这个元素位置
+                            buttons: ['取消', '确认'],
+                            // cancelId: 0,
+                            // checkboxLabel: '确定不去？'
+                        })
+                        console.log(response, checkboxChecked);
+                        // if(response === 1) isMac ? app.hide() : app.quit() 
+                        // if(!checkboxChecked) dialog.showErrorBox('温馨提示', '请勾选复选框')
+                     }
                 }
             ]
         }
