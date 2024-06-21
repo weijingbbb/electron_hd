d8e30c2a2ebfe2d789f2a1077ba2742740eb93b4
weijingbbb <1047129443@qq.com>
2024-06-21 15:49:38 +0800

    摄像头：摄像头页面切换

--- camera/src/renderer/src/App.vue
+++ camera/src/renderer/src/App.vue
@@ -1,12 +1,46 @@
 <script setup lang="ts">
+import { Config, GraphicDesign } from '@icon-park/vue-next';
+import { ref } from 'vue';
+import Camera from './components/Camera.vue';
 import Setting from './components/Setting.vue';
+
+const page = ref('camera')
+
+const changePae = () => {
+  console.log('bbbb')
+  if (page.value === 'camera') {
+    page.value = 'setting'
+  } else {
+    page.value = 'camera'
+  }
+}
 </script>
 
 <template>
   <Suspense>
-    <main>
-    <!-- <Camera></Camera> -->
-    <Setting ></Setting>
+    <main class="relative">
+      <section>
+        <Config
+          v-if="page === 'camera'"
+          theme="outline"
+          size="24"
+          fill="#7ed321"
+          class="absolute z-10 -translate-x-1/2 cursor-pointer left-1/2"
+          @click="changePae"
+        />
+        <graphic-design
+          v-if="page === 'setting'"
+          theme="outline"
+          size="24"
+          fill="#7ed321"
+          class="absolute bottom-0 z-10 -translate-x-1/2 cursor-pointer left-1/2"
+          @click="changePae"
+        />
+      </section>
+      <section>
+        <Camera v-if="page === 'camera'"></Camera>
+        <Setting v-else></Setting>
+      </section>
     </main>
   </Suspense>
 </template>
--- camera/src/renderer/src/components/Setting.vue
+++ camera/src/renderer/src/components/Setting.vue
@@ -1,21 +1,15 @@
 <template>
-  <main class="bg-[#2c3e50] w-screen h-screen">
+  <main class="p-4 bg-[#2c3e50] w-screen h-screen">
     <h2 class="mb-5 text-sm text-center text-gray-50 opacity-70">参数配置</h2>
-    <el-form ref="form" label-width="80px">
-      <el-form-item>
-        <el-select v-model="deviceId" placeholder="选择摄像头">
-          <el-option
-            v-for="item in cameras"
-            :key="item.deviceId"
-            :label="item.label"
-            :value="item.deviceId"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="success" @click="onSubmit">保存</el-button>
-      </el-form-item>
-    </el-form>
+    <el-select v-model="deviceId" placeholder="选择摄像头">
+      <el-option
+        v-for="item in cameras"
+        :key="item.deviceId"
+        :label="item.label"
+        :value="item.deviceId"
+      />
+    </el-select>
+    <el-button type="success" @click="onSubmit" class="w-full mt-4">保存</el-button>
   </main>
 </template>
 <script setup lang="ts">
@@ -24,7 +18,6 @@ import { onMounted, ref } from 'vue';
 const devices = await navigator.mediaDevices.enumerateDevices()
 const cameras = devices.filter((d) => d.kind.includes('video'))
 const deviceId = ref('')
-
 
 console.log(cameras)
 const onSubmit = () => {}
