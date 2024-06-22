eb7811fee2c15f7122b1031cef90f52e227d99d0
weijingbbb <1047129443@qq.com>
2024-06-22 19:51:20 +0800

    片断：解决索引不重置的问题，使用useCallback优化逻辑

--- snippets/src/renderer/src/components/Result/index.tsx
+++ snippets/src/renderer/src/components/Result/index.tsx
@@ -1,5 +1,5 @@
 import useCode from '@renderer/hooks/useCode'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 // import css from './index.module.scss'
 import classNames from 'classnames'
 import './index.scss'
@@ -11,34 +11,39 @@ export default function Result() {
   // 回车敲的项
   const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null)
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    console.log(currentIndex)
-    // 如果没有数据，则无效退出事件
-    if (!data.length) return
-    switch (e.code) {
-      case 'ArrowUp':
-        setCurrentIndex((pre) => (pre - 1 < 0 ? data.length - 1 : pre - 1))
-        break
-      case 'ArrowDown':
-        setCurrentIndex((pre) => (pre + 1 >= data.length ? 0 : pre + 1))
-        break
-      case 'Enter':
-        setHighlightedIndex(currentIndex)
-        // 写入剪贴板
-        navigator.clipboard.writeText(data[currentIndex].content)
-        break
-    }
-  }
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      console.log(currentIndex)
+      // 如果没有数据，则无效退出事件
+      if (!data.length) return
+      switch (e.code) {
+        case 'ArrowUp':
+          setCurrentIndex((pre) => (pre - 1 < 0 ? data.length - 1 : pre - 1))
+          break
+        case 'ArrowDown':
+          setCurrentIndex((pre) => (pre + 1 >= data.length ? 0 : pre + 1))
+          break
+        case 'Enter':
+          setHighlightedIndex(currentIndex)
+          // 写入剪贴板
+          navigator.clipboard.writeText(data[currentIndex].content)
+          break
+      }
+    },
+    [data, currentIndex]
+  )
 
   useEffect(() => {
-    console.log('更新了data')
-    // 每次数据更新后，初始化index
-    // setCurrentIndex(0)
     document.addEventListener('keydown', handleKeyDown)
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
-  }, [data, currentIndex])
+  }, [handleKeyDown])
+
+  useEffect(() => {
+    // 每次数据更新后，初始化index
+    setCurrentIndex(0)
+  }, [data])
 
   return (
     <main className={classNames('result-main')}>
