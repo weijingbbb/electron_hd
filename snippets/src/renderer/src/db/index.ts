18c0dd620fa18663443a64373debda9103927b77
weijingbbb <1047129443@qq.com>
2024-06-25 00:48:14 +0800

    片断：创建数据库连接

--- snippets/src/renderer/src/db/index.ts
+++ snippets/src/renderer/src/db/index.ts
@@ -1,3 +1,11 @@
-import Database from 'better-sqlite3'
-const db = new Database('foobar.db')
+import Database, * as BetterSqlite3 from 'better-sqlite3'
+import { app } from 'electron'
+import { resolve } from 'node:path'
+
+// 这里生成Mac端的路径，放在桌面，win需要重新生成
+const db_name = 'my.db'
+const db_path = resolve(app.getPath('home'), 'Desktop', db_name)
+const db: BetterSqlite3.Database = new Database(db_path)
 db.pragma('journal_mode = WAL')
+
+export { db }
