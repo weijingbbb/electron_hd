cdd27e11a45cec7f9e66cc3652a76204ef66c152
weijingbbb <1047129443@qq.com>
2024-06-24 23:50:19 +0800

    片断：修复编译后窗口打开失败的问题

--- snippets/src/main/config/window.ts
+++ snippets/src/main/config/window.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, shell } from 'electron'
+import url from 'node:url'
 
 import { is } from '@electron-toolkit/utils'
 import { join } from 'path'
@@ -32,9 +33,19 @@ export function createConfigWindow() {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    configWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    configWindow.loadURL(process.env['ELECTRON_RENDERER_URL'] + '/#config')
   } else {
-    configWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    configWindow.loadURL(
+      url.format({
+        // 编译后的文件
+        pathname: join(__dirname, '../renderer/index.html'),
+        // 协议
+        protocol: 'file',
+        slashes: true,
+        // hash的值
+        hash: 'config'
+      })
+    )
   }
 
   // 如果是开发环境，打开开发者工具
