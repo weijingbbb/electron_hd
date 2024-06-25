bdf74a7db930647580b199d897e049a30f56a074
weijingbbb <1047129443@qq.com>
2024-06-25 23:44:28 +0800

    片断：优化加载第一条数据的逻辑

--- snippets/src/renderer/src/pages/content-list/index.tsx
+++ snippets/src/renderer/src/pages/content-list/index.tsx
@@ -1,17 +1,20 @@
 import dayjs from 'dayjs'
-import { useState } from 'react'
-import { NavLink, Outlet, useLoaderData } from 'react-router-dom'
+import { useEffect } from 'react'
+import { NavLink, Outlet, useLoaderData, useNavigate } from 'react-router-dom'
 import './index.scss'
 
 export default function Content() {
   const contents = useLoaderData() as ContentType[]
-  const [content, setContent] = useState<ContentType>()
+  // const [content, setContent] = useState<ContentType>()
 
-  // useEffect(() => {
-  //   if (contents.length) {
-  //     setContent(contents[0])
-  //   }
-  // }, [contents])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (contents.length) {
+      // setContent(contents[0])
+      navigate(`/config/category/content-list/${contents[0].category_id}/content/${contents[0].id}`)
+    }
+  }, [contents])
 
   return (
     <main className="content-list-page">
--- snippets/src/renderer/src/pages/content/index.tsx
+++ snippets/src/renderer/src/pages/content/index.tsx
@@ -4,9 +4,9 @@ import './index.scss'
 export default function Content() {
   const content = useLoaderData() as ContentType
   return (
-    <div className="content-page">
-      <div>{content?.title}</div>
-      <div className=''>{content?.content}</div>
-    </div>
+    <main className="content-page">
+      <div className='h1'>{content?.title}</div>
+      <div className='content'>{content?.content}</div>
+    </main>
   )
 }
