66584fe1e613a67c1da023817af816cb5d347013
weijingbbb <1047129443@qq.com>
2024-06-26 15:36:15 +0800

    片断：添加欢迎页

---
+++ snippets/src/renderer/src/pages/wellcome/index.tsx
@@ -0,0 +1,4 @@
+
+export default function WellCome() {
+  return <main className="flex items-center justify-center h-full bg-slate-50">欢迎</main>
+}
--- snippets/src/renderer/src/router/index.tsx
+++ snippets/src/renderer/src/router/index.tsx
@@ -3,12 +3,12 @@ import Config from '@renderer/pages/config'
 import Content from '@renderer/pages/content'
 import ContentList from '@renderer/pages/content-list'
 import Home from '@renderer/pages/home'
+import WellCome from '@renderer/pages/wellcome'
 import { createHashRouter } from 'react-router-dom'
 
 import CategoryLoader from '@renderer/pages/category/loader'
 import ContentListLoader from '@renderer/pages/content-list/loader'
 import ContentLoader from '@renderer/pages/content/loader'
-
 
 import ContentAction from '@renderer/pages/content/action'
 
@@ -27,8 +27,12 @@ const routers = createHashRouter([
         loader: CategoryLoader,
         children: [
           {
+            index: true, // 这个意思代表如果没有路径匹配，则默认跳转到此页面
+            element: <WellCome />
+          },
+          {
             path: 'content-list/:cid?',
-            // index: true, // 这个意思代表如果没有路径匹配，则默认跳转到此页面
+
             loader: ContentListLoader,
             element: <ContentList />,
             children: [
