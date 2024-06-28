9c1e4f48c34dd9b4b913d695652ad5f25d761657
weijingbbb <1047129443@qq.com>
2024-06-28 22:07:43 +0800

    片断：删除栏目后，重置列表的栏目id

--- snippets/src/renderer/src/pages/category/action.ts
+++ snippets/src/renderer/src/pages/category/action.ts
@@ -2,6 +2,7 @@ import { redirect } from 'react-router-dom'
 
 export default async ({ request }) => {
   const formData = await request.formData()
+  const data = Object.fromEntries(formData)
   const { method } = request
   switch (method) {
     case 'POST': {
@@ -13,8 +14,17 @@ export default async ({ request }) => {
     }
     case 'DELETE': {
       await window.api.sql('delete from categories where id = @id', 'del', {
-        id: formData.get('id')
+        id: data.id
       })
+      // 删除栏目后，把栏目下的内容都设置为未分类
+      await window.api.sql(
+        'update contents set category_id = 0 where category_id = @category_id',
+        'update',
+        {
+          category_id: data.id
+        }
+      )
+      return redirect(`/config/category/content-list`)
     }
   }
   return true
