15c9bbba5d5de61211c7bcf9cc487d99a0780075
weijingbbb <1047129443@qq.com>
2024-06-30 13:48:35 +0800

    片断：对zustand进行状态持久化

--- snippets/src/renderer/src/store/useStore.ts
+++ snippets/src/renderer/src/store/useStore.ts
@@ -1,5 +1,6 @@
 import { DataType } from '@renderer/data'
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface StateProps {
   data: DataType[]
@@ -14,17 +15,25 @@ interface StateProps {
   setEditCategoryId: (id: number) => void
 }
 
-export const useStore = create<StateProps>((set) => {
-  return {
-    data: [],
-    setData: (data) => set({ data }),
-    search: '',
-    setSearch: (search) => set({ search }),
-    error: '',
-    setError: (error) => set({ error }),
-    id: 0,
-    setId: (id) => set({ id }),
-    editCategoryId: 0,
-    setEditCategoryId: (editCategoryId) => set({ editCategoryId })
-  }
-})
+export const useStore = create(
+  persist<StateProps>(
+    (set) => {
+      return {
+        data: [],
+        setData: (data) => set({ data }),
+        search: '',
+        setSearch: (search) => set({ search }),
+        error: '',
+        setError: (error) => set({ error }),
+        id: 0,
+        setId: (id) => set({ id }),
+        editCategoryId: 0,
+        setEditCategoryId: (editCategoryId) => set({ editCategoryId })
+      }
+    },
+    {
+      name: 'wj-hotepad',
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  )
+)
